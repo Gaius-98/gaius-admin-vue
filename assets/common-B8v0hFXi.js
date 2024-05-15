03010e7b07189ece9f3f118c138d028b518fded7
Gaius-98 <2388838628@qq.com>
2024-05-15 03:07:30 +0000

    fix(editTable): watch data to refresh view

---
+++ assets/common-B8v0hFXi.js
@@ -0,0 +1,1 @@
+import{a8 as e}from"./index-Ck1AJYqG.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
--- assets/common-wnDoiM9G.js
+++
@@ -1,1 +0,0 @@
-import{a8 as e}from"./index-D-AsqWGq.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
