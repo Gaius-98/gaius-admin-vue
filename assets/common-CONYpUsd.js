24011f563d6c4330ba634c4d9cbaa56a83455b8d
Gaius-98 <2388838628@qq.com>
2024-05-16 08:55:35 +0000

    fix(auth): refresh page make auth token invalid

---
+++ assets/common-CONYpUsd.js
@@ -0,0 +1,1 @@
+import{a9 as e}from"./index-B5PDD88k.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
--- assets/common-DxcLk8dt.js
+++
@@ -1,1 +0,0 @@
-import{a9 as e}from"./index-CCtK2QNy.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
