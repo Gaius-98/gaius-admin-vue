d320749494ce135ea488dd3c3cf5e30a06e11a00
Gaius-98 <2388838628@qq.com>
2024-05-20 05:50:01 +0000

    chore(eslint): change eslint ignore file

--- assets/common-Bs28BE7o.js
+++
@@ -1,1 +0,0 @@
-import{$ as e}from"./index-598t535-.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
---
+++ assets/common-DKf31Zoy.js
@@ -0,0 +1,1 @@
+import{$ as e}from"./index-SGSUymbp.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
