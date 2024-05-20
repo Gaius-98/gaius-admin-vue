7f28a26328b6e6b7d350d8d4c93d4bd8d189e4cf
Gaius-98 <2388838628@qq.com>
2024-05-20 12:22:33 +0000

    style(global): format code

---
+++ assets/common-Bf0OGs6r.js
@@ -0,0 +1,1 @@
+import{$ as e}from"./index-asFLvw6U.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
--- assets/common-DKf31Zoy.js
+++
@@ -1,1 +0,0 @@
-import{$ as e}from"./index-SGSUymbp.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
