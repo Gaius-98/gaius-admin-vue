b72978dced5cda82d5a63f505d9210880973740d
Gaius-98 <2388838628@qq.com>
2024-05-22 02:15:27 +0000

    fix(dragTableHeader): fix headeritem resize

---
+++ assets/common-D_thIADr.js
@@ -0,0 +1,1 @@
+import{a2 as e}from"./index-DExUugDf.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
--- assets/common-DfDtJCeb.js
+++
@@ -1,1 +0,0 @@
-import{a2 as e}from"./index-Vac1NcXf.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
