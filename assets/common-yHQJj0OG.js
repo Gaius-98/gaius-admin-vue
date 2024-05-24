46b537a06adf65d83d737dc47c40fa47216b41d8
Gaius-98 <2388838628@qq.com>
2024-05-24 03:20:06 +0000

    refactor(schemaForm): change from single  provide el to use global provide plugins

---
+++ assets/common-yHQJj0OG.js
@@ -0,0 +1,1 @@
+import{a3 as e}from"./index-BZm-G3qC.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
--- assets/index-C-FfBvpS.js
+++
@@ -1,1 +0,0 @@
-import{a3 as n,a4 as s}from"./index-BkIIxGqr.js";import{l as o}from"./index-DIkwqaEE.js";const _=o(n),p=o(s);export{_,p as a};
