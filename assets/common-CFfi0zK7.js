fa205f13e733dc598a4e0943a492d37fadf14d85
Gaius-98 <2388838628@qq.com>
2024-05-30 05:54:28 +0000

    fix(layout): layout theme cfg support persistence

---
+++ assets/common-CFfi0zK7.js
@@ -0,0 +1,1 @@
+import{a5 as e}from"./index-CdY1EXP5.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
--- assets/common-D7NZnztR.js
+++
@@ -1,1 +0,0 @@
-import{a5 as e}from"./index-_076D9lD.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
