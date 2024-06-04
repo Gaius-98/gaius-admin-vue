f2ba0d07dcb29fdcdccc6a8044afb1bc7124a2ec
Gaius-98 <2388838628@qq.com>
2024-06-04 07:42:18 +0000

    fix(login): fix login bg image size 100%

--- assets/common-BAtz7NXV.js
+++
@@ -1,1 +0,0 @@
-import{ae as e}from"./index-Dj3POpsG.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
---
+++ assets/common-Cn3zKYyu.js
@@ -0,0 +1,1 @@
+import{ae as e}from"./index-Dnllc660.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
