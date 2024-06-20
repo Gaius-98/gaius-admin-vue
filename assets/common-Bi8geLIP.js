384d72bbd2ba503d589dfa67c6476a5b692213ab
Gaius-98 <2388838628@qq.com>
2024-06-20 03:00:14 +0000

    fix(lowCodeTable): fix lowcodeTable page surplus scroll

---
+++ assets/common-Bi8geLIP.js
@@ -0,0 +1,1 @@
+import{ae as e}from"./index-Dpd769QJ.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
--- assets/common-CL_q1RvU.js
+++
@@ -1,1 +0,0 @@
-import{ae as e}from"./index-BBSwSMIc.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
