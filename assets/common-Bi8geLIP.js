553e2996a773f7f7b05dbaacf6bb6d418e87705b
Gaius-98 <2388838628@qq.com>
2024-06-25 06:55:55 +0000

    fix(lowcode): fix lowcodeEditor page surplus scroll

--- assets/common-Bi8geLIP.js
+++
@@ -1,1 +0,0 @@
-import{ae as e}from"./index-Dpd769QJ.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
---
+++ assets/common-CN7RWgKM.js
@@ -0,0 +1,1 @@
+import{ae as e}from"./index-CRj_7lJZ.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
