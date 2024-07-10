462bb32d325717c962cbbe72925cbab3af98e5b7
Gaius-98 <2388838628@qq.com>
2024-07-10 08:15:30 +0000

    build(global): package optimization

--- assets/eagerComputed-BQoZFmQi.js
+++
@@ -1,1 +0,0 @@
-import{s as t,aC as a}from"./index-CmoA2GoM.js";function r(s){const e=t();return a(()=>{e.value=s()},{flush:"sync"}),e}export{r as e};
--- assets/index-B9RKzsGX.js
+++
@@ -1,1 +0,0 @@
-import{e as n,c as e}from"./index-BYYPv9R6.js";n.Group=e;n.install=function(o){return o.component(n.name,n),o.component(e.name,e),o};
