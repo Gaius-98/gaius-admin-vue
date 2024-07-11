b6f7798e5f7f64bc7b6d7ace6bad9c8a6edf4498
Gaius-98 <2388838628@qq.com>
2024-07-11 11:25:30 +0800

    fix(schemaFomr): fix import custom component

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,18 @@ import 'normalize.css'
 import App from './App.vue'
 import router from './router'
 import { uninstallPreLoad } from './utils/tools'
+import CodeEditor from './components/CodeEditor.vue'
+import ColorPicker from './components/ColorPicker.vue'
+import ImagePicker from './components/ImagePicker.vue'
 import registerSFEl from './components/SchemaForm/registerSFEl'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(registerSFEl,{
-    'code-editor':()=>import('./components/CodeEditor.vue'),
-    'color-picker':()=>import('./components/ColorPicker.vue'),
-    'image-picker':()=>import('./components/ImagePicker.vue')
+    'code-editor':CodeEditor,
+    'color-picker':ColorPicker,
+    'image-picker':ImagePicker
 })
 app.mount('#app')
 uninstallPreLoad()
