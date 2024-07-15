bab10456396868b5da500566e7297c86e5708a4c
Gaius-98 <2388838628@qq.com>
2024-07-15 12:21:16 +0000

    feat(visualDesign): add component form part

--- assets/common-DDtnMbKu.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-RsXN23Tr.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
---
+++ assets/common-sGFF9qDN.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-DUQMv_-S.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
