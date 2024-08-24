2d97769eab6e534947551a7aea0a10ca105a39da
Gaius-98 <2388838628@qq.com>
2024-08-24 11:01:06 +0800

    fix(scheamForm): async import custom component

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
+import { defineAsyncComponent } from 'vue'
 import { createPinia } from 'pinia'
 import 'normalize.css'
 import App from './App.vue'
 import router from './router'
 import { uninstallPreLoad } from './utils/tools'
-import CodeEditor from './components/CodeEditor.vue'
-import ColorPicker from './components/ColorPicker.vue'
-import ImagePicker from './components/ImagePicker.vue'
+
 import registerSFEl from './components/SchemaForm/registerSFEl'
 import './assets/iconfont/iconfont.css'
 import GEchart from '@/components/GEchart/GEchart.vue'
@@ -16,9 +15,9 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(registerSFEl,{
-    'code-editor':CodeEditor,
-    'color-picker':ColorPicker,
-    'image-picker':ImagePicker
+    'code-editor':defineAsyncComponent(()=>import('@/components/CodeEditor.vue')),
+    'color-picker':defineAsyncComponent(()=>import('@/components/ColorPicker.vue')),
+    'image-picker':defineAsyncComponent(()=>import('@/components/ImagePicker.vue'))
 })
 app.component('GEchart',GEchart)
 app.component('VisualGroup',VisualGroup)
