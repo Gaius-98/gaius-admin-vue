6798fe1d2553fe3df48bfd9e25d47ef6753019ed
Gaius-98 <2388838628@qq.com>
2024-09-03 13:15:06 +0000

    feat(table-design): add remove-action-btn logic

---
+++ assets/common-95PMhVPs.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-DOd-Rv4L.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
--- assets/common-BikFPhou.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-CgqLVpwJ.js";const c={getDict:t=>e({method:"get",url:"/dict/dictByType",params:{dictType:t}})};export{c};
